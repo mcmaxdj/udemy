08f11c55acfb370aa7ae1f640780812518808e7a
Maxim Sobetsky <mcmaxdj@gmail.com>
2020-02-09 19:16:51 +0200

    add answers

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var money = prompt('Your monthly budget is?', '');
 var data = prompt('Enter date in YYYY-MM-DD format.', '');
 var days = 30;
-var oneDayBudget = (money - question2) / days;
+var oneDayBudget = parseInt((money - answer2) / days);
 
 var appData = {
   'budget': 'money',
@@ -13,11 +13,11 @@ var appData = {
   savings: false
 };
 
-var question1 = prompt('Enter must-have expenses this month.', '');
-var question2 = prompt('How much it will cost?', '');
+var answer1 = prompt('Enter must-have expenses this month.', '');
+var answer2 = parseInt(prompt('How much it will cost?', ''));
 
-appData.expenses.a = question1;
-appData.expenses.b = question2;
-
+appData.expenses.a = answer1;
+appData.expenses.b = answer2;
+  
 console.log(appData.expenses);
-alert(oneDayBudget);
+alert(parseInt(oneDayBudget));
