bb66e3a0bc321e43a04734e6c4077f52f02344e0
Maxim Sobetsky <mcmaxdj@gmail.com>
2020-10-19 23:11:44 +0300

    update movies script

--- new_script.js
+++ new_script.js
@@ -5,9 +5,55 @@
 // для подобного вывода необходимо использовать косые ковычки
 // console.log(`https://somesite.com/${category}/${forBoys}/21`); // https://somesite.com/toys/boys/21
 
-// Start practic
-let numbersOfFilm = +prompt('How mane movies did you watch?', "");
+// --------------- Start ---------------
+// let num = 21;
+// while (num <= 25) {
+//   console.log(num);
+//   num++;
+// }
 
+// let num2 = 21;
+// do {
+//   console.log(num2);
+//   num2++;
+// }
+// while (num2 <= 25);
+
+// let num3 = 13;
+// for (let i = 1; i < 10; i++) {
+//   if (i == 7) {
+//     break; // оборвет цикл на 7
+//   } 
+//   console.log(i);
+// }
+
+// for (let i = 1; i < 10; i++) {
+//   if (i == 7) {
+//     continue; // пропустит 7
+//   } 
+//   console.log(i);
+// }
+
+// --------------- Start ---------------
+// let num = 21;
+// switch (num) {
+//   case 12:
+//     console.log('no');
+//     break;
+//   case 21:
+//     console.log('ok');
+//     break;
+//   case 1:
+//     console.log('less');
+//     break;
+//   default:
+//     console.log('default');
+//     break;
+// }
+
+// --------------- Start practic ---------------
+
+let numbersOfFilm = +prompt('How mane movies did you watch?', "");
 let personalMoviesDB = {
   count: numbersOfFilm,
   movies: {},
@@ -15,10 +61,14 @@ let personalMoviesDB = {
   genres: [],
   privat: false
 };
+let a = prompt("One of the last viewed movies?", ""),
+    b = prompt("How do you rate it?", ""),
+    c = prompt("One of the last viewed movies?", ""),
+    d = prompt("How do you rate it?", "");
 
-let questionOne = prompt("One of the last viewed movies?", ""),
-    questionTwo = prompt("How do you rate it?", "");
-
-personalMoviesDB.movies[questionOne] = questionTwo;
-
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
 console.log(personalMoviesDB);
+
+
+
